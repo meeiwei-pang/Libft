da368ac7225bb54ba7574115d87e3b6a9263f5b9
Pang Meei Wei <pmeei-we@u82z06s04.42kl.edu.my>
2026-07-27 19:18:38 +0800

    haha

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -10,16 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isdigit(char* str)
+int	ft_isdigit(char c)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (!(str[i] >= '0' && str[i] <= '9')
-			return (0);
-		i++;
-	}
-	return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
